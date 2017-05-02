6aaae24abbc91265e08db7a63e9765f8b37aac63
cindy <mendozaic@hotmail.com>
2017-05-02 16:21:23 -0500

    para empezar validacion

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+function showtx() {
+  document.getElementById('tn').style.display="block";
+  document.getElementById('te').style.display="block";
+  document.getElementById('tc').style.display="block";
+}
