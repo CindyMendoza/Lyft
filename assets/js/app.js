805b17940ff682d3d295d6e72b852f39803e11b3
cindy mendoza <mendozaic@hotmail.com>
2017-10-03 16:42:01 -0500

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,11 +5,7 @@ window.addEventListener('scroll',function(){
 	var a = document.getElementsByClassName('a');
   var login = document.getElementsByClassName('log-in');
 	var singup = document.getElementsByClassName('sing-up');
-	// var signup = document.getElementById('sign-up');
-	// var logoHeader = document.getElementById('logo-header');
-	// var menuList = document.getElementsByClassName('menu-list-a');
-	// var logIn = document.getElementsByClassName('log-in')[0];
- // window.scrollBy(100, 100);
+
 
 console.log(window.pageYOffset);
 console.log(document.documentElement.scrollTop);
@@ -33,28 +29,7 @@ if(window.pageYOffset > 50){
 	logo.setAttribute('src','assets/img/logo-white.png');
 }
 
-	// var accionScroll = window.pageYOffset || document.documentElement.scrollTop;
-	// if (accionScroll > ultimoScrollTop) {
-	// 	navHeader.classList.add('bg-header');
-	// 	logoHeader.setAttribute('src','assets/img/logo-pink.png');
-	// 		signup.style.transform = "translate(-200px, 0px)";
-	// 	for (var i = 0; i < menuList.length; i++) {
-	// 		menuList[i].classList.add('scroll-header-color');
-	// 		logIn.classList.add('scroll-header-border');
-	// 	}
-	// }
-	// else if (accionScroll <= 3) {
-	// 	navHeader.classList.remove('bg-header');
-	// 	logoHeader.setAttribute('src','assets/img/logo-white.png');
-	// 	signup.style.transform = "translate(0px, 0px)";
-	//
-	// 	for (var i = 0; i < menuList.length; i++) {
-	// 		menuList[i].classList.remove('scroll-header-color');
-	// 	}
-	// 	logIn.classList.remove('scroll-header-border');
-	// }
-	// ultimoScrollTop = accionScroll;
-},false);
+	},false);
 
 //desplegar form
 var inputPhone = document.getElementById('phone-number');
